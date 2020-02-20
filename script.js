aa1821feb75f16c94c521b875b9e41609cb0ab5c
leocity <codaleo@gmail.com>
2020-02-20 11:15:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,17 +48,23 @@ particlesJS("particles-js", {
       }
    },
    interactivity: {
-      detect_on: "window",
+      detect_on: "canvas",
       events: {
-         onhover: { enable: false, mode: "grab" },
+         onhover: { enable: false, mode: "repulse" },
          onclick: { enable: true, mode: "push" },
          resize: true
       },
       modes: {
          grab: { distance: 400, line_linked: { opacity: 1 } },
-         bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+         bubble: {
+            distance: 400,
+            size: 40.00000000000001,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+         },
          repulse: { distance: 200, duration: 0.4 },
-         push: { particles_nb: 2 },
+         push: { particles_nb: 4 },
          remove: { particles_nb: 2 }
       }
    },
