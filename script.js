df7d3ab7c7972ad01f19baab537e3f464466a237
leocity <codaleo@gmail.com>
2020-02-20 17:07:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -48,23 +48,17 @@ particlesJS("particles-js", {
       }
    },
    interactivity: {
-      detect_on: "canvas",
+      detect_on: "window",
       events: {
-         onhover: { enable: false, mode: "repulse" },
+         onhover: { enable: false, mode: "grab" },
          onclick: { enable: true, mode: "push" },
          resize: true
       },
       modes: {
          grab: { distance: 400, line_linked: { opacity: 1 } },
-         bubble: {
-            distance: 400,
-            size: 40.00000000000001,
-            duration: 2,
-            opacity: 8,
-            speed: 3
-         },
+         bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
          repulse: { distance: 200, duration: 0.4 },
-         push: { particles_nb: 4 },
+         push: { particles_nb: 2 },
          remove: { particles_nb: 2 }
       }
    },
